f6ba3b3b0ec25bd934a52209f495144967937101
madhukavirajith <madvira99@gmail.com>
2026-04-13 14:21:06 +0530

    Remove invalid app.options('*', cors()) line

--- server/server.js
+++ server/server.js
@@ -52,7 +52,6 @@ app.use(cors({
 }));
 
 // Pre-flight OPTIONS request handling
-app.options('*', cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
